6ecc4aa480273d232b4a62f881309bbe4d389afe
Ivà Martí Suñer <imarti@nuek.com>
2026-05-19 10:25:14 +0200

    format js

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ console.log("home created");
 
 //Feature de login
 
-function login(){
+function login() {
     //function para el login
     console.log("login");
 }
 
-function camera(){
+function camera() {
     //function para abrir la camara
 
 }
